f9b79cdd92f3da42c346faa6e055438dd614ac98
Julia Jakimowicz <julia.jakimowicz@edu.uekat.pl>
2025-01-10 12:30:52 +0100

    Dodano nowe pliki zad_2b1.py i zad_2b2.py

---
+++ zad_2b1.py
@@ -0,0 +1,7 @@
+def pomnozone_przez_2(liczby):
+  rezultat = []
+  for liczba in liczby:
+    rezultat.append(liczba*2)
+  return rezultat
+
+print(pomnozone_przez_2([3,6,4,7,8]))
---
+++ zad_2b2.py
@@ -0,0 +1,4 @@
+def pomnozone_przez_2(liczby):
+    return [liczba * 2 for liczba in liczby]
+
+print(pomnozone_przez_2([3,6,4,7,2]))
