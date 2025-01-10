a7c3aec23c181c82773b2b1178aeadc2c791fda4
Julia Jakimowicz <julia.jakimowicz@edu.uekat.pl>
2025-01-10 12:29:52 +0100

    Usunięto stary plik zad_2b.py

--- zad_2b.py
+++
@@ -1,9 +0,0 @@
-def multiply_by_two_for(numbers):
-    result = []
-    for number in numbers:
-        result.append(number * 2)
-    return result
-
-
-lista = [1, 2, 3, 4, 5]
-print(multiply_by_two_for(lista))
