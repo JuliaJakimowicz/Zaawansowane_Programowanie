678a4d629c01eba7c51911cf12b8f51c9703fd1e
Julia Jakimowicz <julia.jakimowicz@edu.uekat.pl>
2025-01-14 12:14:30 +0100

    Poprawiono błędy zgodności z PEP 8

--- Magazine/Order.py
+++ Magazine/Order.py
@@ -1,5 +1,6 @@
 from Magazine import utils
 
+
 def utworz_zamowienie(cena, stawka_podatku):
 
     cena_calosciowa = cena + utils.oblicz_podatek(cena, stawka_podatku)
--- Magazine/Product.py
+++ Magazine/Product.py
@@ -1,5 +1,6 @@
 from Magazine import utils
 
+
 def cena_z_podatkiem(cena, stawka_podatku):
 
     return cena + utils.oblicz_podatek(cena, stawka_podatku)
